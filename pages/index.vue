5f4128c1c2853aac0a95860781615706b5f911c6
staaph <contact.staaph@gmail.com>
2022-06-18 20:31:31 +0200

    update colormode

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>Color mode: {{ $colorMode.value }}</h1>
-    <select v-model="$colorMode.preference">
-      <option value="system">System</option>
-      <option value="light">Light</option>
-      <option value="dark">Dark</option>
-      <option value="sepia">Sepia</option>
-    </select>
+    <button
+      v-if="$colorMode.preference === 'light'"
+      @click="$colorMode.preference = 'dark'"
+    >
+      dark
+    </button>
+    <button
+      v-if="$colorMode.preference === 'dark'"
+      @click="$colorMode.preference = 'light'"
+    >
+      light
+    </button>
   </div>
 </template>
 
