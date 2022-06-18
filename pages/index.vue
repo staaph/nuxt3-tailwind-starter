81d28255ea11e8d02b8a90fa929073b7fe0d9077
staaph <contact.staaph@gmail.com>
2022-06-18 20:47:19 +0200

    added color selector

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,30 @@
 <template>
   <div>
     <h1>Color mode: {{ $colorMode.value }}</h1>
-    <button
-      v-if="$colorMode.preference == 'light'"
-      @click="$colorMode.preference = 'dark'"
-    >
-      dark
-    </button>
-    <button
-      v-if="$colorMode.preference == 'dark'"
-      @click="$colorMode.preference = 'light'"
-    >
-      light
-    </button>
+    <select v-model="$colorMode.preference">
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+      <option value="sepia">Sepia</option>
+    </select>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 const colorMode = useColorMode();
-console.log(colorMode.preference);
 </script>
+
+<style>
+body {
+  background-color: #fff;
+  color: rgba(0, 0, 0, 0.8);
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
+</style>
