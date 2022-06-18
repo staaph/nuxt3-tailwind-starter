d730444b228f37a89a218f6c890dcf5ed9673041
staaph <contact.staaph@gmail.com>
2022-06-18 20:34:46 +0200

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,8 @@
 <template>
   <div>
     <h1>Color mode: {{ $colorMode.value }}</h1>
-    <button
-      v-if="$colorMode.preference === 'light'"
-      @click="$colorMode.preference = 'dark'"
-    >
-      dark
-    </button>
-    <button
-      v-if="$colorMode.preference === 'dark'"
-      @click="$colorMode.preference = 'light'"
-    >
-      light
-    </button>
+    <button @click="$colorMode.preference = 'dark'">dark</button>
+    <button @click="$colorMode.preference = 'light'">light</button>
   </div>
 </template>
 
